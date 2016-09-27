8cb1a95b7ce77854dc35e24e25c9103e860a27ff
apple <meixinghao@365yf.com>
2016-09-27 16:00:56 +0800

    xiaochengxu

---
+++ xiaochengxu/page/component/component-pages/button/button.js
@@ -0,0 +1,55 @@
+var types = ['default', 'primary', 'warn']
+var pageObject = {
+  data: {
+    defaultSize: 'default',
+    primarySize: 'default',
+    warnSize: 'default',
+    disabled: false,
+    plain: false,
+    loading: false
+  },
+  setDisabled: function(e) {
+    this.setData({
+      disabled: !this.data.disabled
+    })
+  },
+  setPlain: function(e) {
+    this.setData({
+      plain: !this.data.plain
+    })
+  },
+  setLoading: function(e) {
+    this.setData({
+      loading: !this.data.loading
+    })
+  },
+  request: function(e) {
+      console.log(e.currentTarget.id);
+      var self = this
+      wx.request({
+          url: 'http://local.yueai1.com/touch/home/register',
+          data:{},
+          success: function(res) {
+              self.setData({
+                  loading: res.data.a
+              })
+              console.log(res);
+          }
+
+      })
+  }
+}
+
+for (var i = 0; i < types.length; ++i) {
+  (function(type) {
+    pageObject[type] = function(e) {
+      var key = type + 'Size'
+      var changedData = {}
+      changedData[key] =
+        this.data[key] === 'default' ? 'mini' : 'default'
+      this.setData(changedData)
+    }
+  })(types[i])
+}
+
+Page(pageObject)
