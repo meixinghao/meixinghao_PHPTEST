d00215ae42ff997aec3a32e4030d7496d371ca7f
梅兴昊 <meixinghao@365yf.com>
2017-02-13 16:51:39 +0800

    测试

---
+++ PhpTestProjects/test_unset_array_Arg.php
@@ -0,0 +1,14 @@
+<?php
+
+//$test = ['a' => 1, 'b' => 2, 'c' => 3];
+//
+//print_r($test);
+//
+//unset($test['a']);
+//
+//print_r($test);
+
+while ($pos = strrpos('_name', '_')) {
+    echo "hello";
+//    return;
+}
